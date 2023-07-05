8f22a15c7ac542c0d27c8626d97639611f9cfc0f
fanxin li <lexxshinjj@gmail.com>
2023-07-05 21:58:02 +0800

    routes 1.0

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::RecipesController < Api::V1::BaseController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :recipes, only: %i[show create index update]
+      resources :meals, only: %i[show create index]
+      resources :goals, only: %i[show create index]
+    end
+  end
 end
