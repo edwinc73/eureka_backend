436a9aab58e2839d0a8ca930755c35f864dea933
fanxin li <lexxshinjj@gmail.com>
2023-07-05 22:14:57 +0800

    calories in meal is unnessary

--- db/migrate/20230705014420_create_meals.rb
+++ db/migrate/20230705014420_create_meals.rb
@@ -4,7 +4,6 @@ class CreateMeals < ActiveRecord::Migration[7.0]
       t.references :user, null: false, foreign_key: true
       t.references :recipe, null: false, foreign_key: true
       t.string :name
-      t.integer :calories
 
       t.timestamps
     end
