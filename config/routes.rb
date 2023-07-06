22e12d265cdf9593e93d3e59af4bad1c632728ca
fanxin li <lexxshinjj@gmail.com>
2023-07-06 19:18:51 +0800

    version 1.0 routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :recipes, only: %i[show create index update]
+      resources :recipes, only: %i[show create index update] do
+        resources :favourite_recipes, only: :create
+        resources :reviews, only: %i[create index]
+      end
       resources :meals, only: %i[show create index]
       resources :goals, only: %i[show create index]
+      resources :favourite_recipes, only: :index
     end
   end
 end
