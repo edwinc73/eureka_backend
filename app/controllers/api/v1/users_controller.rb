6a78ad367134f39c078b4f34b5902f93bf30d52d
fanxin li <lexxshinjj@gmail.com>
2023-07-08 20:13:47 +0800

    you can update the users information including age, weight, height, gender

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,25 @@ class Api::V1::UsersController < Api::V1::BaseController
     # @user = @current_user
     @user = User.last
   end
+
+  def update
+    # @user = @current_user
+    @user = User.last
+    if @user.update(user_params)
+      render json: { msg: 'user information changed' }
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:age, :weight, :height, :gender)
+  end
+
+  def render_error
+    render json: { errors: @user.errors.full_messages },
+    status: :unprocessable_entity
+  end
 end
