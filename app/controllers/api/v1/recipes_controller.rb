efbfdacf31f2662ed6f25c16505cd7bb4b27040b
fanxin li <lexxshinjj@gmail.com>
2023-07-10 14:31:06 +0800

    search function for recipes and ingredients

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::RecipesController < Api::V1::BaseController
   def index
-    @recipes = Recipe.includes(:prep, :ingredients).all
+    if params[:query].present?
+      search_query = "%#{params[:query]}%"
+      @recipes = Recipe.where('LOWER(name) LIKE LOWER(?)', search_query)
+      @ingredients = Ingredient.where('LOWER(name) LIKE LOWER(?)', search_query)
+    else
+      @recipes = Recipe.all
+    end
+
   end
 
   def upload_img
