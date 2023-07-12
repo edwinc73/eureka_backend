3c8cecad31da233e7d211acfd28d4bdb549c4ea5
fanxin li <lexxshinjj@gmail.com>
2023-07-12 21:56:27 +0800

    serializer completed

--- app/controllers/api/v1/favourite_recipes_controller.rb
+++ app/controllers/api/v1/favourite_recipes_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::FavouriteRecipesController < ApplicationController
+class Api::V1::FavouriteRecipesController < Api::V1::BaseController
   def index
     # user = @current_user.id
     user = User.last
@@ -14,7 +14,7 @@ class Api::V1::FavouriteRecipesController < ApplicationController
   end
 
   def destroy
-    recipe = Recipe.find(params[:id])
+    recipe = Recipe.find(9)
     # user = @current_user.id
     user = User.last
     user.recipes.delete(recipe)
