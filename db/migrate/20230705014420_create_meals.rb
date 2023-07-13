19185ccae67ffdaba41f2a2823bc784215b32d65
fanxin li <lexxshinjj@gmail.com>
2023-07-13 21:37:21 +0800

    debug

--- db/migrate/20230705014420_create_meals.rb
+++ db/migrate/20230705014420_create_meals.rb
@@ -3,6 +3,7 @@ class CreateMeals < ActiveRecord::Migration[7.0]
     create_table :meals do |t|
       t.references :goal, null: false, foreign_key: true
       t.references :recipe, null: false, foreign_key: true
+      t.float :portion
       t.string :name
 
       t.timestamps
