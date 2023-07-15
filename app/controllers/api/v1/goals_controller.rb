0b58a6ebd680d1e628d5dad810d564982d42c315
fanxin li <lexxshinjj@gmail.com>
2023-07-15 15:31:32 +0800

    change goals

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::GoalsController < Api::V1::BaseController
   before_action :set_goal, only: %i[update]
-  before_action :update_current_goal, only: %i[show]
+  #before_action :update_current_goal, only: %i[show]
   def show
+    # @goal = @current_user.goals.last
+    @goal = User.last.goals.last
   end
 
   def index
@@ -29,21 +31,21 @@ class Api::V1::GoalsController < Api::V1::BaseController
     @goal = Goal.find(params[:id])
   end
 
-  def update_current_goal
-    #@goal = @current_user.goals.last
-    @goal = User.last.goals.last
-    calorie_meals = @goal.meals.map do |meal|
-      meal.recipe.total_calories / meal.recipe.portion * meal.portion
-    end
-    fat_meals = @goal.meals.map { |meal| meal.recipe.fat / meal.recipe.portion * meal.portion }
-    protein_meals = @goal.meals.map { |meal| meal.recipe.protein / meal.recipe.portion * meal.portion }
-    carbs_meals = @goal.meals.map { |meal| meal.recipe.carbs / meal.recipe.portion * meal.portion }
-    @goal.current_fat = fat_meals.sum.round(1)
-    @goal.current_protein = protein_meals.sum.round(1)
-    @goal.current_carbs = carbs_meals.sum.round(1)
-    @goal.current_calorie = calorie_meals.sum
-    @goal.save
-  end
+  # def update_current_goal
+    # @goal = @current_user.goals.last
+    # @goal = User.last.goals.last
+    # calorie_meals = @goal.meals.map do |meal|
+      # meal.recipe.total_calories / meal.recipe.portion * meal.portion
+    # end
+    # fat_meals = @goal.meals.map { |meal| meal.recipe.fat / meal.recipe.portion * meal.portion }
+    # protein_meals = @goal.meals.map { |meal| meal.recipe.protein / meal.recipe.portion * meal.portion }
+    # carbs_meals = @goal.meals.map { |meal| meal.recipe.carbs / meal.recipe.portion * meal.portion }
+    # @goal.current_fat = fat_meals.sum.round(1)
+    # @goal.current_protein = protein_meals.sum.round(1)
+    # @goal.current_carbs = carbs_meals.sum.round(1)
+    # @goal.current_calorie = calorie_meals.sum
+    # @goal.save
+  # end
 
   def render_error
     render json: { errors: @goal.errors.full_messages },
