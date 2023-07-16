c1fe9e8776465a61ccf4be6e596005e1281d2ec9
fanxin li <lexxshinjj@gmail.com>
2023-07-16 13:29:44 +0800

    more recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -336,6 +336,240 @@ recipes = [
     sodium: 300,
     category: "Breakfast",
     photo: "https://www.cookingclassy.com/wp-content/uploads/2018/12/roasted-vegetables-10.jpg"
+  },
+  {
+    name: "Spaghetti Bolognese",
+    description: "Classic Italian pasta dish with a rich meat sauce",
+    instructions: "1. Heat olive oil in a large saucepan over medium heat.\n2. Add chopped onions, garlic, and minced beef to the saucepan. Cook until the meat is browned.\n3. Stir in tomato paste, canned tomatoes, and herbs. Simmer for 30 minutes.\n4. Cook spaghetti according to package instructions.\n5. Serve the Bolognese sauce over cooked spaghetti. Sprinkle with grated Parmesan cheese.\n6. Garnish with fresh basil leaves.",
+    total_calories: 400,
+    fat: 20,
+    protein: 25,
+    carbs: 35,
+    fiber: 5,
+    sodium: 500,
+    category: "Main Dish",
+    photo: "https://www.recipetineats.com/wp-content/uploads/2018/07/Spaghetti-Bolognese.jpg"
+  },
+  {
+    name: "Chicken Stir-Fry",
+    description: "Quick and easy stir-fry with tender chicken and colorful vegetables",
+    instructions: "1. Heat oil in a wok or large skillet over high heat.\n2. Add sliced chicken and cook until browned and cooked through.\n3. Remove the chicken from the wok and set aside.\n4. Add chopped vegetables (such as bell peppers, carrots, and broccoli) to the wok. Stir-fry until crisp-tender.\n5. Return the chicken to the wok and toss with the vegetables.\n6. In a small bowl, mix together soy sauce, ginger, garlic, and cornstarch. Pour over the chicken and vegetables.\n7. Cook for another 1-2 minutes until the sauce thickens.\n8. Serve the stir-fry over steamed rice.",
+    total_calories: 350,
+    fat: 10,
+    protein: 30,
+    carbs: 40,
+    fiber: 8,
+    sodium: 600,
+    category: "Main Dish",
+    photo: "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2021/05/Chicken-Stir-Fry-9.jpg"
+  },
+  {
+    name: "Avocado Toast",
+    description: "Simple and delicious toast topped with mashed avocado",
+    instructions: "1. Toast slices of bread until golden brown.\n2. In a bowl, mash ripe avocados with a fork. Season with salt, pepper, and a squeeze of lemon juice.\n3. Spread the mashed avocado onto the toasted bread.\n4. Optional: Top with additional ingredients like sliced tomatoes, feta cheese, or a drizzle of olive oil.\n5. Serve as a quick and nutritious breakfast or snack.",
+    total_calories: 200,
+    fat: 15,
+    protein: 5,
+    carbs: 10,
+    fiber: 7,
+    sodium: 150,
+    category: "Breakfast",
+    photo: "https://www.eatingwell.com/thmb/YwNw19g19tS31P21KvIBFo1mVFk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1807w-avocado-toast-recipe-8029771-2000-aefaa92c11e74e80b0bfc15788a61465.jpg"
+  },
+  {
+    name: "Greek Salad",
+    description: "Traditional Mediterranean salad with fresh vegetables and feta cheese",
+    instructions: "1. Chop cucumber, tomatoes, red onion, and bell peppers into bite-sized pieces.\n2. In a large bowl, combine the chopped vegetables, Kalamata olives, and crumbled feta cheese.\n3. Drizzle with olive oil and red wine vinegar.\n4. Season with dried oregano, salt, and pepper.\n5. Toss to combine all the ingredients.\n6. Serve as a refreshing side dish or light lunch.",
+    total_calories: 250,
+    fat: 20,
+    protein: 10,
+    carbs: 15,
+    fiber: 5,
+    sodium: 400,
+    category: "Salad",
+    photo: "https://www.simplyrecipes.com/thmb/Mo0HabY7Va_kEJtbtVmjdCqMzsI=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2011__08__dads-greek-salad-vertical-a-1600-d67fd37678a64815996f56e7ec02d452.jpg"
+  },
+  {
+    name: "Mango Smoothie",
+    description: "Creamy and tropical smoothie made with ripe mangoes",
+    instructions: "1. Peel and dice ripe mangoes.\n2. In a blender, combine diced mangoes, Greek yogurt, honey, and a splash of orange juice.\n3. Blend until smooth and creamy.\n4. Optional: Add ice cubes for a chilled smoothie.\n5. Pour the smoothie into glasses and garnish with fresh mint leaves.\n6. Enjoy as a refreshing breakfast or snack.",
+    total_calories: 250,
+    fat: 5,
+    protein: 8,
+    carbs: 50,
+    fiber: 6,
+    sodium: 50,
+    category: "Beverage",
+    photo: "https://www.acouplecooks.com/wp-content/uploads/2020/06/Mango-Smoothie-007.jpg"
+  },
+  {
+    name: "Quinoa Salad",
+    description: "Healthy and nutritious salad with cooked quinoa, vegetables, and a tangy dressing",
+    instructions: "1. Cook quinoa according to package instructions and let it cool.\n2. Chop vegetables such as cucumbers, cherry tomatoes, bell peppers, and red onions.\n3. In a large bowl, combine the cooked quinoa and chopped vegetables.\n4. In a small jar, mix together olive oil, lemon juice, Dijon mustard, and salt.\n5. Drizzle the dressing over the quinoa and vegetables.\n6. Toss to combine all the ingredients.\n7. Optional: Garnish with fresh herbs like parsley or cilantro.\n8. Serve chilled as a satisfying and flavorful salad.",
+    total_calories: 300,
+    fat: 10,
+    protein: 8,
+    carbs: 45,
+    fiber: 6,
+    sodium: 250,
+    category: "Salad",
+    photo: "https://cookieandkate.com/images/2017/08/best-quinoa-salad-recipe-3.jpg"
+  },
+  {
+    name: "Pesto Pasta",
+    description: "Delicious pasta dish tossed in a flavorful basil pesto sauce",
+    instructions: "1. Cook pasta according to package instructions until al dente.\n2. In a food processor, blend fresh basil leaves, pine nuts, garlic, Parmesan cheese, and olive oil until smooth.\n3. Drain the cooked pasta and return it to the pot.\n4. Add the prepared pesto sauce to the pasta and toss to coat.\n5. Optional: Add cooked chicken or roasted vegetables for extra flavor and texture.\n6. Serve the pesto pasta hot and garnish with additional Parmesan cheese and fresh basil.",
+    total_calories: 400,
+    fat: 18,
+    protein: 12,
+    carbs: 50,
+    fiber: 4,
+    sodium: 350,
+    category: "Main Dish",
+    photo: "https://www.recipetineats.com/wp-content/uploads/2019/02/Pesto-Pasta_2-1.jpg"
+  },
+  {
+    name: "Berry Smoothie Bowl",
+    description: "Colorful and refreshing smoothie bowl topped with fresh berries and granola",
+    instructions: "1. In a blender, combine frozen mixed berries, banana, Greek yogurt, and a splash of almond milk.\n2. Blend until smooth and creamy.\n3. Pour the smoothie into a bowl.\n4. Top with fresh berries, sliced banana, granola, and a drizzle of honey.\n5. Optional: Add chia seeds or flaxseeds for added nutrition.\n6. Enjoy the smoothie bowl as a nourishing breakfast or snack.",
+    total_calories: 300,
+    fat: 6,
+    protein: 8,
+    carbs: 55,
+    fiber: 10,
+    sodium: 100,
+    category: "Beverage",
+    photo: "https://minimalistbaker.com/wp-content/uploads/2016/07/The-PERFECT-5-minute-Smoothie-Bowl-Simple-ingredients-naturally-sweet-SO-healthy-vegan-glutenfree-smoothiebowl-recipe.jpg"
+  },
+  {
+    name: "Fruit Salad",
+    description: "Colorful and refreshing salad with a variety of fresh fruits",
+    instructions: "1. Chop fruits such as watermelon, pineapple, strawberries, grapes, and kiwi into bite-sized pieces.\n2. In a large bowl, combine the chopped fruits.\n3. Optional: Add a squeeze of lemon or lime juice to prevent the fruits from browning.\n4. Toss the fruits gently to combine.\n5. Optional: Add a drizzle of honey or a sprinkle of mint leaves for extra flavor.\n6. Chill the fruit salad in the refrigerator for at least 30 minutes before serving.\n7. Serve the fruit salad as a refreshing and healthy dessert or snack.",
+    total_calories: 150,
+    fat: 1,
+    protein: 2,
+    carbs: 38,
+    fiber: 5,
+    sodium: 5,
+    category: "Salad",
+    photo: "https://hips.hearstapps.com/hmg-prod/images/fruit-salad-vertical-jpg-1522181929.jpg?crop=0.696xw:0.464xh;0.252xw,0.286xh&resize=980:*"
+  },
+  {
+    name: "Grilled Chicken Caesar Salad",
+    description: "Classic Caesar salad with grilled chicken breast",
+    instructions: "1. Season chicken breast with salt, pepper, and your choice of herbs.\n2. Grill the chicken breast until cooked through.\n3. Slice the grilled chicken into thin strips.\n4. In a large bowl, combine romaine lettuce, croutons, grated Parmesan cheese, and Caesar dressing.\n5. Toss the salad ingredients to coat them with the dressing.\n6. Top the salad with the sliced grilled chicken.\n7. Serve the grilled chicken Caesar salad as a hearty and flavorful meal.",
+    total_calories: 350,
+    fat: 18,
+    protein: 30,
+    carbs: 20,
+    fiber: 4,
+    sodium: 450,
+    category: "Salad",
+    photo: "https://static01.nyt.com/images/2015/06/17/dining/17PAIR2/17PAIR2-articleLarge.jpg?w=1280&q=75"
+  },
+  {
+    name: "Margherita Pizza",
+    description: "Classic Italian pizza topped with fresh tomatoes, mozzarella, and basil",
+    instructions: "1. Preheat the oven to the highest temperature.\n2. Roll out the pizza dough into a round shape.\n3. Place the pizza dough on a baking sheet or pizza stone.\n4. Spread tomato sauce evenly over the pizza dough.\n5. Top with slices of fresh mozzarella cheese and tomato slices.\n6. Sprinkle with salt, pepper, and fresh basil leaves.\n7. Bake the pizza in the preheated oven for about 10-12 minutes or until the crust is golden and crispy.\n8. Remove from the oven and let it cool slightly before slicing.\n9. Serve the Margherita pizza hot and enjoy!",
+    total_calories: 300,
+    fat: 12,
+    protein: 14,
+    carbs: 36,
+    fiber: 2,
+    sodium: 550,
+    category: "Main Dish",
+    photo: "https://cookieandkate.com/images/2021/07/margherita-pizza-recipe-1-2-1097x1650.jpg"
+  },
+  {
+    name: "Spinach and Feta Stuffed Chicken Breast",
+    description: "Juicy chicken breast filled with spinach and feta cheese",
+    instructions: "1. Preheat the oven to 400°F (200°C).\n2. Butterfly the chicken breasts by making a lengthwise cut, but not cutting all the way through.\n3. Season the inside of each chicken breast with salt and pepper.\n4. In a bowl, mix together chopped spinach, crumbled feta cheese, minced garlic, and a drizzle of olive oil.\n5. Stuff the chicken breasts with the spinach and feta mixture.\n6. Secure the openings with toothpicks.\n7. Place the stuffed chicken breasts on a baking sheet.\n8. Bake in the preheated oven for 25-30 minutes or until the chicken is cooked through.\n9. Remove the toothpicks before serving.",
+    total_calories: 400,
+    fat: 18,
+    protein: 45,
+    carbs: 5,
+    fiber: 2,
+    sodium: 550,
+    category: "Poultry",
+    photo: "https://feelgoodfoodie.net/wp-content/uploads/2017/03/Spinach-_-Feta-Stuffed-Chicken-11.jpg"
+  },
+  {
+    name: "Kung Pao Chicken",
+    description: "Spicy and flavorful stir-fried chicken dish with peanuts and vegetables",
+    instructions: "1. Marinate diced chicken breast with soy sauce, rice vinegar, and cornstarch for about 20 minutes.\n2. In a wok or large skillet, heat oil over high heat.\n3. Add dried red chili peppers and stir-fry for a few seconds to release the heat.\n4. Add the marinated chicken and stir-fry until cooked through.\n5. Add chopped vegetables such as bell peppers, zucchini, and carrots. Stir-fry for a few more minutes.\n6. In a small bowl, mix together soy sauce, rice vinegar, sugar, and cornstarch to make the sauce.\n7. Pour the sauce over the chicken and vegetables. Stir-fry until the sauce thickens.\n8. Garnish with roasted peanuts and green onions.\n9. Serve the Kung Pao Chicken with steamed rice.",
+    total_calories: 400,
+    fat: 18,
+    protein: 30,
+    carbs: 25,
+    fiber: 4,
+    sodium: 800,
+    category: "Main Dish",
+    photo: "https://cafedelites.com/wp-content/uploads/2018/04/Best-Kung-Pao-Chicken-IMAGE-2.jpg"
+  },
+  {
+    name: "Mapo Tofu",
+    description: "Classic Sichuan dish with silky tofu and minced pork in a spicy sauce",
+    instructions: "1. In a wok or large skillet, heat oil over medium heat.\n2. Add minced garlic and ginger, and sauté until fragrant.\n3. Add minced pork and cook until browned.\n4. Stir in doubanjiang (spicy bean paste) and fermented black beans.\n5. Add tofu cubes and gently stir to coat them with the sauce.\n6. Pour in a mixture of chicken broth and cornstarch to thicken the sauce.\n7. Add diced green onions and Sichuan peppercorns.\n8. Simmer for a few minutes until the flavors are well combined.\n9. Serve the Mapo Tofu over steamed rice.",
+    total_calories: 350,
+    fat: 20,
+    protein: 15,
+    carbs: 30,
+    fiber: 4,
+    sodium: 700,
+    category: "Main Dish",
+    photo: "https://thewoksoflife.com/wp-content/uploads/2019/06/mapo-tofu-10.jpg"
+  },
+  {
+    name: "Steamed Dumplings (Jiaozi)",
+    description: "Traditional Chinese dumplings filled with a savory mixture of meat and vegetables",
+    instructions: "1. In a large bowl, combine ground pork, minced garlic, minced ginger, chopped scallions, soy sauce, sesame oil, and salt.\n2. Mix well to evenly distribute the ingredients.\n3. Place a spoonful of the filling onto a dumpling wrapper.\n4. Fold the wrapper in half and pinch the edges to seal the dumpling.\n5. Repeat with the remaining filling and wrappers.\n6. Arrange the dumplings in a steamer basket lined with parchment paper.\n7. Steam the dumplings over high heat for about 10-12 minutes, or until cooked through.\n8. Serve the Steamed Dumplings with soy sauce and vinegar dipping sauce.",
+    total_calories: 250,
+    fat: 10,
+    protein: 12,
+    carbs: 30,
+    fiber: 2,
+    sodium: 600,
+    category: "Appetizer",
+    photo: "https://rasamalaysia.com/wp-content/uploads/2009/07/steamed_dumplings2.jpg"
+  },
+  {
+    name: "Classic Cheeseburger",
+    description: "Juicy beef patty topped with cheese, lettuce, tomato, and condiments, served in a bun",
+    instructions: "1. Preheat the grill or stovetop griddle.\n2. Divide ground beef into equal-sized portions and shape them into patties.\n3. Season the patties with salt and pepper.\n4. Cook the patties on the grill or griddle, flipping them once, until desired doneness.\n5. Place a slice of cheese on each patty and let it melt.\n6. Toast the burger buns on the grill or in a toaster.\n7. Assemble the burgers by placing a patty on the bottom bun.\n8. Add lettuce, tomato slices, onion slices, and any desired condiments.\n9. Top with the other half of the bun.\n10. Serve the Classic Cheeseburgers with fries or a side of your choice.",
+    total_calories: 500,
+    fat: 30,
+    protein: 25,
+    carbs: 35,
+    fiber: 3,
+    sodium: 800,
+    category: "Main Dish",
+    photo: "https://iambaker.net/wp-content/uploads/2019/05/cheeseburger-1.jpg"
+  },
+  {
+    name: "Teriyaki Chicken",
+    description: "Grilled chicken glazed with a sweet and savory teriyaki sauce",
+    instructions: "1. In a small bowl, mix together soy sauce, mirin, sugar, and ginger to make the teriyaki sauce.\n2. Marinate chicken thighs in the teriyaki sauce for at least 30 minutes.\n3. Preheat the grill or stovetop griddle.\n4. Grill the chicken thighs until cooked through, basting with more teriyaki sauce.\n5. Remove the chicken from the grill and let it rest for a few minutes.\n6. Slice the chicken into strips.\n7. Serve the Teriyaki Chicken over steamed rice, garnished with green onions and sesame seeds.\n8. Optional: Add a side of steamed vegetables or a salad.",
+    total_calories: 300,
+    fat: 10,
+    protein: 25,
+    carbs: 30,
+    fiber: 2,
+    sodium: 600,
+    category: "Main Dish",
+    photo: "https://www.the-girl-who-ate-everything.com/wp-content/uploads/2022/09/teriyaki-chicken-2.jpg"
+  },
+  {
+    name: "Bibimbap",
+    description: "A nutritious and flavorful Korean rice bowl with assorted toppings",
+    instructions: "1. Cook rice according to package instructions.\n2. Prepare assorted toppings such as sautéed vegetables (spinach, carrots, mushrooms), cooked beef, sliced cucumber, bean sprouts, and fried egg.\n3. Arrange the cooked rice in a bowl.\n4. Place the toppings in separate sections on top of the rice.\n5. Optional: Drizzle with gochujang (Korean red chili paste) for added spiciness.\n6. Mix all the ingredients together before eating to combine the flavors.\n7. Serve the Bibimbap hot and enjoy!",
+    total_calories: 400,
+    fat: 12,
+    protein: 20,
+    carbs: 60,
+    fiber: 5,
+    sodium: 800,
+    category: "Main Dish",
+    photo: "https://www.recipetineats.com/wp-content/uploads/2019/05/Bibimbap_3.jpg"
   }
 ]
 
@@ -397,7 +631,7 @@ Prep.create!(
   recipe: pasta_carbonara
 )
 
-20.times do
+50.times do
   FavouriteRecipe.create!(user: User.all.sample, recipe: Recipe.all.sample)
 end
 
@@ -453,7 +687,7 @@ end
 #empty the today's meals
 User.last.goals.last.meals.destroy_all
 
-150.times do
+200.times do
   content = ["This recipe is a delightful combination of flavors that will tantalize your taste buds and leave you craving for more.",
     "Prepare to be impressed by the mouthwatering flavors and textures of this dish that will surely satisfy your culinary desires.",
     "Indulge in the perfect harmony of ingredients that create a truly satisfying and memorable culinary experience.",
