57b6af04cbab55694da90ee86170e5694ee4d163
fanxin li <lexxshinjj@gmail.com>
2023-07-17 12:30:45 +0800

    1

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -21,7 +21,7 @@ class Api::V1::RecipesController < Api::V1::BaseController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.created_by = User.last # @current_user.id
+    @recipe.created_by_id = User.last.id # @current_user.id
     if @recipe.save
       render json: @recipe, status: :created
     else
