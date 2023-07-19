9b98b29c8c590941b5260632a034694e93d50df0
fanxin li <lexxshinjj@gmail.com>
2023-07-19 10:36:23 +0800

    debug

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::IngredientsController < Api::V1::BaseController
   def index
-    params[:query] = "eg"
+    # params[:query] = "eg"
     if params[:query].present?
       search_query = "%#{params[:query]}%"
       @ingredients = Ingredient.where('LOWER(name) LIKE LOWER(?) OR LOWER(category) LIKE LOWER(?)', search_query, search_query)
