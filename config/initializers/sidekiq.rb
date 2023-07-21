52d474055448178084fbd696f114c574317338c1
Edwin Cheng <edwincheng09@googlemail.com>
2023-07-22 05:23:02 +0800

    scalingo sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,15 @@
-require 'sidekiq/cron'
+# require 'sidekiq/cron'
 
-schedule_file = "config/sidekiq_schedule.yml"
+# schedule_file = "config/sidekiq_schedule.yml"
 
-if File.exist?(schedule_file)
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+# if File.exist?(schedule_file)
+#   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+# end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
 end
