10e006adc7250a825c9813a13c7369a52f5a1545
fanxin li <lexxshinjj@gmail.com>
2023-07-22 11:21:55 +0800

    create user image in seed

--- db/schema.rb
+++ db/schema.rb
@@ -151,6 +151,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_132936) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "image"
     t.integer "age"
     t.float "weight"
     t.integer "height"
