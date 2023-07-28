a1ba8927ddfd600817c37abf0a298f1543c30164
fanxin li <lexxshinjj@gmail.com>
2023-07-28 09:27:09 +0800

    tst

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::SessionsController < Api::V1::BaseController
-  skip_before_action :verify_request, only: [:login]
+  #skip_before_action :verify_request, only: [:login]
 
   def login
     user = find_user
